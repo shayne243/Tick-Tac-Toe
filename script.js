ed6db01d17a0ce7658d819b07bffa73174dba634
shayne243 <marques0@purdue.edu>
2020-06-29 16:36:24 +0400

    Grid clicks done and symbols appearing

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let form = document.getElementById("player-form");
 let gameGrid = document.getElementById("game");
+let gridList = gameGrid.getElementsByTagName("h1");
+let inputList = form.getElementsByTagName("input");
+let instruct = document.getElementById("instruct");
+let player = ["X", 1];
 let playerOne = new Player();
 let playerTwo = new Player();
 let game = new Game();
@@ -22,19 +26,32 @@ function createPlayers(nameOne, nameTwo) {
 
 function createGame() {
     game.roundNo++;
-    let inputList = form.getElementsByTagName("input");
     createPlayers(inputList[0].value, inputList[1].value);
     console.log(playerOne, playerTwo);
     form.setAttribute("style", "display:none");
     gameGrid.setAttribute("style", "opacity: 1;");
+    instruct.setAttribute("style", "display: block");
     form.reset();
 }
 
 function setupGrid() {
-    let gridList = gameGrid.getElementsByTagName("h1");
     for (let i = 0; i < gridList.length; ++i) {
         gridList[i].dataset.boxNumber = i + 1;
-        gridList[i].innerHTML = i + 1;
+        gridList[i].innerHTML = "_";
+        gridList[i].addEventListener("click", ()=> {
+            if (gridList[i].innerHTML == "_") {
+                gridList[i].innerHTML = player[0];
+                if (player[0] == "X") {
+                    player[0] = "O";
+                    player[1] = 2
+                }
+                else {
+                    player[0] = "X";
+                    player[1] = 1;
+                }
+                instruct.innerHTML = "Player " + player[1] + " make your move...";
+            }
+        });
     }
 }
 
